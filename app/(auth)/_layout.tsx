fa86638331b77930864ca8b020d62fb74dfaff05
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-11-05 16:51:28 +0530

    add font

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+import {Stack} from "expo-router";
+
+const AuthLayout = () => {
+    return (
+        <Stack screenOptions={{ headerShown: false, animation: "ios" }}>
+            <Stack.Screen name="emailSignin"/>
+            <Stack.Screen name="profileInfo"/>
+            <Stack.Screen name="signupCompletion"/>
+        </Stack>
+    )
+}
+
+export default AuthLayout
