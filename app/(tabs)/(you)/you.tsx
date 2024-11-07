6d1862e97ada61137174b8672f3f8f711470265d
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-11-07 22:06:46 +0530

    complete profile screen

--- app/(tabs)/(you)/you.tsx
+++ app/(tabs)/(you)/you.tsx
@@ -1,12 +1,17 @@
-import ScreenLayout from "@/components/ScreenLayout";
-import {View, Text, TextInput, TouchableOpacity, Image, ScrollView} from 'react-native';
-import {MediumText, RegularText, SemiBoldText} from "@/components/StyledText";
+import {View, TouchableOpacity, Image, ScrollView} from 'react-native';
+import {LightText, MediumText, RegularText, SemiBoldText} from "@/components/StyledText";
+import {Feather, FontAwesome6} from "@expo/vector-icons";
+import {useRouter} from "expo-router";
 
 const YouScreen = () => {
+    const router = useRouter();
 
     return(
         <ScrollView className='w-full h-full px-4'>
-            <TouchableOpacity className='h-64 px-6 py-10 bg-white rounded-2xl flex justify-between mt-4'>
+            <TouchableOpacity
+                className='h-64 px-6 py-10 bg-white rounded-2xl flex justify-between mt-4'
+                onPress={() => router.push('profile')}
+            >
                 <Image source={require('../../../assets/images/logo_black.png')} className='w-12 h-12'/>
                 <View>
                     <SemiBoldText className='text-4xl'>Vinidu M</SemiBoldText>
@@ -19,18 +24,35 @@ const YouScreen = () => {
                 <MediumText className='text-2xl mb-4'>Community</MediumText>
                 <ScrollView className='' horizontal={true}>
                     <View className='mr-2'>
-                        <Image source={require('../../../assets/images/community/family.png')} className='object-cover object-center'/>
+                        <Image source={require('../../../assets/images/community/family.png')} className='w-36 h-48 object-cover object-center'/>
                     </View>
                     <View className='mr-2'>
-                        <Image source={require('../../../assets/images/community/family.png')} className='object-cover object-center'/>
+                        <Image source={require('../../../assets/images/community/firends.png')} className='w-36 h-48 object-cover object-center'/>
                     </View>
                     <View className='mr-2'>
-                        <Image source={require('../../../assets/images/community/family.png')} className='object-cover object-center'/>
-                    </View>
-                    <View className='mr-2'>
-                        <Image source={require('../../../assets/images/community/family.png')} className='object-cover object-center'/>
+                        <Image source={require('../../../assets/images/community/groups.png')} className='w-36 h-48 object-cover object-center'/>
                     </View>
                 </ScrollView>
+            </View>
+
+            <View className='mt-6'>
+                <MediumText className='text-2xl mb-4'>Goals</MediumText>
+                <View className='flex-row'>
+                    <TouchableOpacity className='bg-white w-40 h-56 p-6 mr-4 flex justify-between rounded-2xl'>
+                        <FontAwesome6 name="person-running" size={28} color="black" />
+                        <View>
+                            <MediumText className='text-xl'>Activity</MediumText>
+                            <LightText className='text-lg'>5 goals</LightText>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity className='bg-white w-40 h-56 p-6 flex justify-between rounded-2xl'>
+                        <Feather name="activity" size={28} color="black" />
+                        <View>
+                            <MediumText className='text-xl'>Health</MediumText>
+                            <LightText className='text-lg'>2 goals</LightText>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScrollView>
     )
