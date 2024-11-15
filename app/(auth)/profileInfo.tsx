7610f8a5910ffc929cab78a0647c4afdfac63ff0
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-11-15 11:16:04 +0530

    complete in profileinfo

--- app/(auth)/profileInfo.tsx
+++ app/(auth)/profileInfo.tsx
@@ -3,31 +3,76 @@ import {View, Text, TextInput, TouchableOpacity, Image} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import ScreenLayout from "@/components/ScreenLayout";
 import {Href, useRouter} from "expo-router";
+import {LightText, MediumText, RegularText, SemiBoldText} from "@/components/StyledText";
+import {ProfileInfoFormValidation} from "@/util/validation";
+import {ALERT_TYPE, Dialog, Toast} from "react-native-alert-notification";
+import {updateUser} from "@/service/userService";
+import {useAppContext} from "@/context/AppContext";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const ProfileInfoScreen = () => {
     const [height, setHeight] = useState('');
     const [weight, setWeight] = useState('');
-    const [sex, setSex] = useState('');
+    const [gender, setGender] = useState('');
+    const [isLoading, setLoading] = useState(false);
     const router = useRouter();
+    const {userData, updateUserData} = useAppContext();
+
+
+
+    const handleSubmit = () => {
+        try {
+            if (ProfileInfoFormValidation(height, weight, gender)){
+                setLoading(true);
+                updateUser(userData?.uid, {
+                    height: parseInt(height),
+                    weight: parseInt(weight),
+                    gender: gender
+                }).then((res: any) => {
+                    const userData = res.data;
+                    updateUserData(userData);
+
+                    setLoading(false);
+                    router.replace('/(auth)/signupCompletion' as Href);
+                }).catch((error) => {
+                    setLoading(false);
+                    Dialog.show({
+                        type: ALERT_TYPE.DANGER,
+                        title: 'Danger',
+                        textBody: `${error.message}`,
+                        button: 'close',
+                    })
+                })
+            }
+        } catch (error: any) {
+            Toast.show({
+                type: ALERT_TYPE.WARNING,
+                title: 'Warning',
+                textBody: error.message,
+                autoClose: 2000,
+            });
+        }
+    }
 
     return (
         <ScreenLayout bgColor='background'>
-            <View className='w-full h-full flex justify-between py-6 px-4'>
+            <Spinner visible={isLoading} color='#08B9AF'/>
+            <View className='w-full h-full flex justify-between mt-4 py-10 px-4'>
                 <View>
                     <View className="flex justify-start items-start mb-6">
                         <Image source={require('../../assets/images/info_icon.png')} className='w-10 h-10'/>
                     </View>
 
                     <View>
-                        <Text className="text-2xl font-bold mb-2">Add Fitbit profile info</Text>
-                        <Text className="text-sm text-[#757575] mb-8">
+                        <SemiBoldText className="text-3xl mb-2">Add Fitbit profile info</SemiBoldText>
+                        <LightText className="text-sm text-[#757575] mb-8">
                             Fitbit uses this information to calculate some metrics like stride length and speed. To choose
                             who sees this, go to Fitbit settings {'>'} Social & Sharing {'>'} Privacy. Information in your
                             profile is private by default.
-                        </Text>
+                        </LightText>
 
                         <View className="mb-5">
-                            <Text className="mb-2 text-grayText">Your profile info</Text>
+                            <MediumText className="mb-2 text-grayText">Your profile info</MediumText>
 
                             <View className="flex-row justify-between mb-4">
                                 <TextInput
@@ -36,6 +81,7 @@ const ProfileInfoScreen = () => {
                                     placeholder="Height"
                                     keyboardType="numeric"
                                     className="border border-[#E0E0E0] rounded p-2 flex-1 mr-2 bg-white"
+                                    style={{fontFamily: 'Assistant_400Regular'}}
                                 />
                                 <TextInput
                                     value={weight}
@@ -43,36 +89,37 @@ const ProfileInfoScreen = () => {
                                     placeholder="Weight"
                                     keyboardType="numeric"
                                     className="border border-[#E0E0E0] rounded p-2 flex-1 bg-white"
+                                    style={{fontFamily: 'Assistant_400Regular'}}
                                 />
                             </View>
 
                             <View className="border border-[#E0E0E0] rounded bg-white">
                                 <Picker
-                                    selectedValue={sex}
-                                    onValueChange={(itemValue) => setSex(itemValue)}
-                                    style={{height: 50}}
+                                    selectedValue={gender}
+                                    onValueChange={(itemValue) => setGender(itemValue)}
+                                    style={{height: 50, fontFamily: 'Assistant_400Regular'}}
                                 >
-                                    <Picker.Item label="Sex" value=""/>
-                                    <Picker.Item label="Male" value="male"/>
-                                    <Picker.Item label="Female" value="female"/>
+                                    <Picker.Item label="Gender" value=""/>
+                                    <Picker.Item label="Male" value="Male"/>
+                                    <Picker.Item label="Female" value="Female"/>
                                 </Picker>
                             </View>
 
-                            <Text className="text-xs text-grayText mt-2">
+                            <RegularText className="text-xs text-grayText mt-2">
                                 Fitbit uses sex to calculate metrics like calories burned, and to provide reference points
                                 you can use for comparison.
-                            </Text>
+                            </RegularText>
                         </View>
                     </View>
                 </View>
 
                 <View className="flex-row justify-between items-center pl-4">
                     <TouchableOpacity>
-                        <Text className="text-[#2E7D32] text-sm">Exit</Text>
+                        <RegularText className="text-[#2E7D32] text-sm">Exit</RegularText>
                     </TouchableOpacity>
 
-                    <TouchableOpacity className="bg-primary p-3 rounded-full" onPress={() => router.push('(auth)/signupCompletion' as Href)}>
-                        <Text className="text-white text-sm">Save & continue</Text>
+                    <TouchableOpacity className="bg-primary p-3 rounded-full" onPress={handleSubmit}>
+                        <RegularText className="text-white text-sm">Save & continue</RegularText>
                     </TouchableOpacity>
                 </View>
             </View>
--- app/(auth)/signupCompletion.tsx
+++ app/(auth)/signupCompletion.tsx
@@ -1,29 +1,30 @@
 import ScreenLayout from "@/components/ScreenLayout";
 import {Text, View, Image, TouchableOpacity} from "react-native";
 import React from "react";
-import {useRouter} from "expo-router";
+import {Href, useRouter} from "expo-router";
+import {RegularText, SemiBoldText} from "@/components/StyledText";
 
 const SignupCompletion = () => {
     const router = useRouter();
 
     return (
         <ScreenLayout bgColor='background'>
-            <View className='w-full h-full flex justify-between px-4 py-6'>
+            <View className='w-full h-full flex justify-between mt-4 px-4 py-10'>
                 <View>
                     <Image source={require('../../assets/images/tick.png')} className='w-10 h-10 mb-6'/>
-                    <Text className='text-2xl mb-2'>You're ready to go</Text>
-                    <Text className='text-sm text-grayText'>
+                    <SemiBoldText className='text-3xl mb-2'>You're ready to go</SemiBoldText>
+                    <RegularText className='text-sm text-grayText'>
                         Start using the Fitbit app on your phone to record your steps, track your
                         exercise, log your meals, and more
-                    </Text>
+                    </RegularText>
                 </View>
                 <View className='flex justify-center items-center'>
                     <Image source={require('../../assets/images/complettion_img.png')} className='w-80 h-96'/>
                 </View>
 
                 <View className='flex-row justify-end'>
-                    <TouchableOpacity className='bg-[#006B5B] w-fit px-6 py-2.5 rounded-3xl' onPress={() => router.replace('(tabs)')}>
-                        <Text className='text-white'>Done</Text>
+                    <TouchableOpacity className='bg-[#006B5B] w-fit px-6 py-2.5 rounded-3xl' onPress={() => router.replace('/(tabs)' as Href)}>
+                        <RegularText className='text-white'>Done</RegularText>
                     </TouchableOpacity>
                 </View>
             </View>
