bfc59085fa0b0d7f1a10ab15810ef700474457fa
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-11-15 11:18:08 +0530

    changes in AppContext

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,8 +1,12 @@
 import React, {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import auth, {FirebaseAuthTypes} from "@react-native-firebase/auth";
 import {Href, useRouter} from "expo-router";
+import {getUserByUid} from "@/service/userService";
+import {handleConnectionError} from "@/util/errors";
 
 interface AppContextType {
+    userData: User | null;
+    updateUserData: (userData: User) => void;
     logoutUser : () => void;
 }
 
@@ -14,22 +18,41 @@ const AppContext = createContext<AppContextType | undefined>(undefined);
 
 export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children }) => {
     const [user, setUser] = useState<FirebaseAuthTypes.User | null>(auth().currentUser);
+    const [userData, setUserData] = useState<User | null>(null);
     const router = useRouter();
 
     useEffect(() => {
         if (user) {
-            router.replace('/(tabs)');
+            getUserByUid(user.uid)
+                .then((res: any) => {
+                    const userData = res.data;
+                    setUserData(userData);
+                    router.replace('/(tabs)' as Href);
+                })
+                .catch((error) => {
+                    if (error.response.status === 404) {
+                        auth().signOut()
+                            .then(() => router.replace('/(auth)/welcome' as Href))
+                            .catch(() => handleConnectionError)
+                    } else {
+                        handleConnectionError();
+                    }
+                })
         } else {
             router.replace('/(auth)/welcome' as Href);
         }
     }, [user]);
+
+    const updateUserData = (userData: User) => {
+        setUserData(userData);
+    }
 
     const logoutUser = () => {
         setUser(null);
     }
 
     return (
-        <AppContext.Provider value={{ logoutUser }}>
+        <AppContext.Provider value={{ userData, updateUserData, logoutUser }}>
             {children}
         </AppContext.Provider>
     );
