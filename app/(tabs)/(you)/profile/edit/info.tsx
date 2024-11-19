5cdc7a1759bd090ba3f6b655e4e8745d390772c1
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-11-19 13:35:27 +0530

    complete profile info edit screen

--- app/(tabs)/(you)/profile/edit/info.tsx
+++ app/(tabs)/(you)/profile/edit/info.tsx
@@ -2,17 +2,67 @@ import {Text, TextInput, TouchableOpacity, View} from "react-native";
 import {Picker} from "@react-native-picker/picker";
 import React, {useState} from "react";
 import {LightText, MediumText, RegularText, SemiBoldText} from "@/components/StyledText";
+import {useAppContext} from "@/context/AppContext";
+import {ProfileInfoFormValidation} from "@/util/validation";
+import {updateUser} from "@/service/userService";
+import {Href, useRouter} from "expo-router";
+import {ALERT_TYPE, Dialog, Toast} from "react-native-alert-notification";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const EditProfileInfoScreen = () => {
-    const [height, setHeight] = useState('');
-    const [weight, setWeight] = useState('');
-    const [sex, setSex] = useState('');
+    const {userData, updateUserData} = useAppContext();
+    const [height, setHeight] = useState(String(userData?.height || ''));
+    const [weight, setWeight] = useState(String(userData?.weight || ''));
+    const [gender, setGender] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const handleSubmit = () => {
+        try {
+            if (ProfileInfoFormValidation(height, weight, gender)){
+                setLoading(true);
+                updateUser(userData?.uid, {
+                    height: parseInt(height),
+                    weight: parseInt(weight),
+                    gender: gender
+                }).then((res: any) => {
+                    const userData = res.data;
+                    updateUserData(userData);
+                    setLoading(false);
+
+                    router.replace('/profile' as Href);
+                    Toast.show({
+                        type: ALERT_TYPE.SUCCESS,
+                        title: 'Warning',
+                        textBody: 'Profile information update successful',
+                        autoClose: 2000,
+                    });
+                }).catch((error) => {
+                    setLoading(false);
+                    Dialog.show({
+                        type: ALERT_TYPE.DANGER,
+                        title: 'Danger',
+                        textBody: `${error.message}`,
+                        button: 'close',
+                    })
+                })
+            }
+        } catch (error: any) {
+            Toast.show({
+                type: ALERT_TYPE.WARNING,
+                title: 'Warning',
+                textBody: error.message,
+                autoClose: 2000,
+            });
+        }
+    }
 
     return (
         <View className='w-full h-full px-4'>
+            <Spinner visible={isLoading} color='#08B9AF'/>
             <View className='flex-row justify-between pr-4 mt-2 mb-4'>
                 <SemiBoldText className='text-xl'>Profile Information</SemiBoldText>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleSubmit}>
                     <RegularText className='text-lg text-tintGreen'>Save</RegularText>
                 </TouchableOpacity>
             </View>
@@ -44,8 +94,8 @@ const EditProfileInfoScreen = () => {
 
                 <View className="border border-[#E0E0E0] rounded bg-white">
                     <Picker
-                        selectedValue={sex}
-                        onValueChange={(itemValue) => setSex(itemValue)}
+                        selectedValue={gender}
+                        onValueChange={(itemValue) => setGender(itemValue)}
                         style={{height: 50}}
                     >
                         <Picker.Item label="Sex" value=""/>
